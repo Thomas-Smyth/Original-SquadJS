9dc860a050607ba71a7afa5baba04dfbd26a6a23
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-02-18 14:14:20 +0000

    Sample Index

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
+// Import Dependencies
+const Discord = require('discord.js');
+
+// Init Discord Bot
+const client = new Discord.Client();
+
+// Import App and SquadServer Classes
 const src = process.env.NODE_ENV === 'production' ? './build' : './src';
 const ServerManager = require(src).Application;
 const SquadServer = require(src).SquadServer;
 
-const SeedingMessage = require(src).SeedingMessage;
+// Import Plugins
+const SeedingMessagePlugin = require(src).SeedingMessage;
+const DiscordStatusMessagePlugin = require(src).DiscordStatusMessage;
+const LayerSelectorPlugin = require(src).LayerSelector;
+const MapVotePlugin = require(src).MapVote;
 
+// Init App
 const app = new ServerManager();
-const seeder = new SeedingMessage();
 
-app.addPlugin(seeder.plugin());
+// Add Servers to App
+const ServerName = new SquadServer(
+  'ServerName',
+  'IP Addr',
+  7787,
+  27165,
+  'Rcon Password'
+);
+app.addServer(ServerName);
 
-app.addServer(new SquadServer());
+// Init Plugins
+const SeedingMessage = new SeedingMessagePlugin();
 
-app.watch();
+const LayerSelector = new LayerSelectorPlugin();
+LayerSelector.layerFilter();
+
+const MapVote = new MapVotePlugin(client, LayerSelector, {
+  'Map Vote Channel ID': ServerName
+});
+
+const DiscordStatusMessage = new DiscordStatusMessagePlugin(client, {
+  ServerName: [{ channelID: 'Channel ID', messageID: 'Message ID' }]
+});
+
+// Add Plugins
+app.addPlugin(SeedingMessage.plugin());
+app.addPlugin(LayerSelector.plugin());
+app.addPlugin(MapVote.plugin());
+app.addPlugin(DiscordStatusMessage.plugin());
+
+// Start App
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  app.watch();
+  MapVote.newVoteMessage(ServerName.id);
+});
+
+// Login Bot
+client.login('Discord Bot Login Token');
