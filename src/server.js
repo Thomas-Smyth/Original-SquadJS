e97066a642d6bebeaf66dd85eab900c64496f74a
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-05-04 14:38:08 +0100

    Better error catch

--- src/server.js
+++ src/server.js
@@ -45,76 +45,85 @@ export default class SquadServer {
    * @returns {Promise<void>}
    */
   async getServerInfo() {
-    // A2S Query
-    let response = await Gamedig.query({
-      type: 'protocol-valve',
-      host: this.host,
-      port: this.queryPort
-    });
-
-    this.name = response.name;
-
-    this.maxPlayers = parseInt(response.maxplayers);
-    this.publicSlots = parseInt(response.raw.rules.NUMPUBCONN);
-    this.reserveSlots = parseInt(response.raw.rules.NUMPRIVCONN);
-
-    this.players = response.players;
-    this.populationCount = Math.min(this.maxPlayers, response.players.length);
-    this.populationHistory.unshift(this.populationCount);
-    this.populationHistory.slice(0, this.populationHistoryMaxLength);
-    this.populationGrowth = isNaN(this.populationHistory[5])
-      ? undefined
-      : this.populationHistory[0] - this.populationHistory[5];
-
-    this.publicQueue = parseInt(response.raw.rules.PublicQueue_i);
-    this.reserveQueue = parseInt(response.raw.rules.ReservedQueue_i);
-
-    this.matchTimeout = parseFloat(response.raw.rules.MatchTimeout_f);
-    this.gameVersion = response.raw.version;
-
-    // RCON Query
-    this.layerChange = false;
-    this.nextLayerChange = false;
-    let { currentLayer, nextLayer } = await this.rcon.getCurrentAndNextLayer();
-
-    // Ignore transitional maps
-    if (
-      currentLayer === '/Game/Maps/TransitionMap' ||
-      nextLayer === '/Game/Maps/TransitionMap'
-    ) {
-      currentLayer = this.currentLayer;
-      nextLayer = this.nextLayer;
-    }
-
-    if (this.currentLayer !== undefined && this.currentLayer !== currentLayer)
-      this.layerChange = true;
-
-    if (this.layerChange || this.currentLayer === undefined) {
-      this.layerHistory.unshift({
-        layer: currentLayer,
-        time: new Date()
+    try {
+      // A2S Query
+      let response = await Gamedig.query({
+        type: 'protocol-valve',
+        host: this.host,
+        port: this.queryPort
       });
-      this.layerHistory.slice(0, this.layerHistoryMaxLength);
+
+      this.name = response.name;
+
+      this.maxPlayers = parseInt(response.maxplayers);
+      this.publicSlots = parseInt(response.raw.rules.NUMPUBCONN);
+      this.reserveSlots = parseInt(response.raw.rules.NUMPRIVCONN);
+
+      this.players = response.players;
+      this.populationCount = Math.min(this.maxPlayers, response.players.length);
+      this.populationHistory.unshift(this.populationCount);
+      this.populationHistory.slice(0, this.populationHistoryMaxLength);
+      this.populationGrowth = isNaN(this.populationHistory[5])
+        ? undefined
+        : this.populationHistory[0] - this.populationHistory[5];
+
+      this.publicQueue = parseInt(response.raw.rules.PublicQueue_i);
+      this.reserveQueue = parseInt(response.raw.rules.ReservedQueue_i);
+
+      this.matchTimeout = parseFloat(response.raw.rules.MatchTimeout_f);
+      this.gameVersion = response.raw.version;
+
+      // RCON Query
+      this.layerChange = false;
+      this.nextLayerChange = false;
+      let {
+        currentLayer,
+        nextLayer
+      } = await this.rcon.getCurrentAndNextLayer();
+
+      // Ignore transitional maps
+      if (
+        currentLayer === '/Game/Maps/TransitionMap' ||
+        nextLayer === '/Game/Maps/TransitionMap'
+      ) {
+        currentLayer = this.currentLayer;
+        nextLayer = this.nextLayer;
+      }
+
+      if (this.currentLayer !== undefined && this.currentLayer !== currentLayer)
+        this.layerChange = true;
+
+      if (this.layerChange || this.currentLayer === undefined) {
+        this.layerHistory.unshift({
+          layer: currentLayer,
+          time: new Date()
+        });
+        this.layerHistory.slice(0, this.layerHistoryMaxLength);
+      }
+
+      this.currentLayer = currentLayer;
+      this.originalCurrentLayer = currentLayer;
+
+      if (
+        this.nextLayer !== undefined &&
+        this.layerChange === false &&
+        this.nextLayer !== nextLayer
+      )
+        this.nextLayerChange = true;
+      this.nextLayer = nextLayer;
+      this.originalNextLayer = nextLayer;
+
+      if (
+        this.lastNextLayerUpdate !== undefined &&
+        this.lastNextLayerUpdate !== this.nextLayer
+      )
+        this.adminChangeNextLayer = true;
+      if (this.layerChange === true) this.adminChangeNextLayer = false;
+    } catch (err) {
+      console.log(`${this.id} throw an error when fetching server information`);
+      this.layerChange = false;
+      this.nextLayerChange = false;
     }
-
-    this.currentLayer = currentLayer;
-    this.originalCurrentLayer = currentLayer;
-
-    if (
-      this.nextLayer !== undefined &&
-      this.layerChange === false &&
-      this.nextLayer !== nextLayer
-    )
-      this.nextLayerChange = true;
-    this.nextLayer = nextLayer;
-    this.originalNextLayer = nextLayer;
-
-    if (
-      this.lastNextLayerUpdate !== undefined &&
-      this.lastNextLayerUpdate !== this.nextLayer
-    )
-      this.adminChangeNextLayer = true;
-    if (this.layerChange === true) this.adminChangeNextLayer = false;
   }
 
   /**
@@ -123,21 +132,25 @@ export default class SquadServer {
    * @returns {Promise<void>}
    */
   async setServerInfo() {
-    if (this.originalCurrentLayer !== this.currentLayer)
-      await this.rcon.changeLayer(this.currentLayer);
-    if (this.originalNextLayer !== this.nextLayer) {
-      // Used to tell if the admin has updated the layer via other means since the time the script was run.
-      this.lastNextLayerUpdate = this.nextLayer;
-      await this.rcon.setNextLayer(this.nextLayer);
+    try {
+      if (this.originalCurrentLayer !== this.currentLayer)
+        await this.rcon.changeLayer(this.currentLayer);
+      if (this.originalNextLayer !== this.nextLayer) {
+        // Used to tell if the admin has updated the layer via other means since the time the script was run.
+        this.lastNextLayerUpdate = this.nextLayer;
+        await this.rcon.setNextLayer(this.nextLayer);
+      }
+
+      this.announcements.forEach((message, delay) => {
+        setTimeout(async () => {
+          await this.rcon.makeAnnouncement(message);
+        }, delay * 10000);
+      });
+
+      this.announcements = [];
+    } catch (err) {
+      console.log(`${this.id} throw an error when updating server information`);
     }
-
-    this.announcements.forEach((message, delay) => {
-      setTimeout(async () => {
-        await this.rcon.makeAnnouncement(message);
-      }, delay * 10000);
-    });
-
-    this.announcements = [];
   }
 
   /**
