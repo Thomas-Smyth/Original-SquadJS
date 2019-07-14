df98e7ef0d2a46820c0096aae87efd639240ab87
Thomas Smyth <thomas.smyth3141@gmail.com>
2019-07-14 13:41:30 +0100

    pm2 Support

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'SquadJS',
+      script: 'index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: { NODE_ENV: 'development' },
+      env_production: { NODE_ENV: 'production' }
+    }
+  ]
+};
